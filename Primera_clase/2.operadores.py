bf9c40a003128f36a2870e3726690948052574bb
Miguel Troya <miguel.troyat@unac.edu.co>
2021-03-17 14:59:49 -0500

    clase 1

---
+++ Primera_clase/2.operadores.py
@@ -0,0 +1,25 @@
+def main():
+    oper1 = 4
+    oper2 = 5
+#aritmeticos:
+    print(oper1 + oper2)
+    print(oper1 - oper2)
+    print(oper1 * oper2)
+    print(oper1 / oper2)
+    print(oper1 % oper2)
+    print(oper1 ** oper2)
+    print(oper1 // oper2)
+
+#comparacion (return bool):
+    print(oper1 > oper2)    
+    print(oper1 < oper2) 
+    print(oper1 == oper2)
+    print(oper1 != oper2)
+    print(oper1 >= oper2)
+    print(oper1 <= oper2)
+    
+
+
+
+
+main()
---
+++ Primera_clase/3.cadenas.py
@@ -0,0 +1,26 @@
+def main():
+    
+    Nombre_Completo = "Miguel Troya"
+
+    print(Nombre_Completo[5])
+    print(Nombre_Completo[0:6])
+
+    concatenacion = "i" + " love" + " music"
+    print(concatenacion)
+
+    #metodo len
+    print(len(Nombre_Completo))
+
+    #metodo upper() / lower()
+    print(Nombre_Completo.upper())
+    print(Nombre_Completo.lower())
+
+    #metodo split
+    print(Nombre_Completo.split(" "))
+
+
+
+
+main() 
+
+
