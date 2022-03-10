b701555ea613cfb589861c865e3121de5e9cae70
Miguel Troya <80295526+TroyaMi@users.noreply.github.com>
2022-03-10 12:04:06 -0500

    Add files via upload

---
+++ GorupJS/clase3.10.2022.js
@@ -0,0 +1,21 @@
+console.log('Ahora si es verdad que estoy programando')
+
+//var nombre = ' Miguel '
+//var apellido = 'Troya'
+
+//var nombreCompleto = nombre + apellido
+//console.log(nombreCompleto)
+
+//console.log('Tu nombre completo es:' + nombreCompleto)
+
+//alert('Tu nombre completo es:' + nombreCompleto)
+
+//var nombre = prompt('Digite su nombre:')
+//var estadoCivil = prompt('Digite tu estado civil: ')
+
+//alert('Nombre: ' + nombre + ' de estado civil: ' + estadoCivil)
+
+var number1 = prompt('Digite un numero')
+var number2 = prompt('Digite un segundo numero')
+var suma = parseInt(number1) + parseInt(number2)
+alert(suma)
